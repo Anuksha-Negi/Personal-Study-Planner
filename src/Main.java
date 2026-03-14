de196e45a4e4f4e959fd711f4d591e539460313f
Anuksha <anuksha790@gmail.com>
2026-03-14 14:39:14 +0530

    Added basic menu and add task feature

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,22 @@ public class Main {
         int choice;
         do{
             System.out.println("\nAdd your task");
-
             System.out.println("1 Add Task");
-            System.out.println("2 Exit");
+            System.out.println("2 View Task");
+            System.out.println("3 Mark task completed");
+            System.out.println("4 delete the completed task");
+            System.out.println("5 Exit");
 
             choice = scanner.nextInt();
+            scanner.nextLine();
+            if(choice == 5){
+                addTask();
+            }
         }while(choice != 2);
+    }
+    static void addTask(){
+        System.out.println("enter your Study task:");
+        String task=scanner.nextLine();
+        System.out.println("Task Added:"+task);
     }
 }
